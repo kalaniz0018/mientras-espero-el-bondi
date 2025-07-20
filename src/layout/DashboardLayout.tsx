07b357feebbddcecb0800f33d93fe981642de61d
kalaniz <kalaniz@telecentro.net.ar>
2025-07-19 22:23:25 -0300

    removiendo dispobnibilidad de modificar tamaño a los widget

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -7,37 +7,29 @@ import { Efemerides } from "../components/Efemerides";
 import { NextFeriado } from "../components/NextFeriado";
 
 export const DashboardLayout: React.FC = () => {
-  const layout = [
-    { i: "clock", x: 0, y: 0, w: 3, h: 1 },
-    { i: "weather", x: 3, y: 0, w: 3, h: 2 },
-    { i: "news", x: 0, y: 2, w: 6, h: 2 },
-    { i: "efemerides", x: 6, y: 0, w: 6, h: 2 },
-    { i: "feriado", x: 6, y: 2, w: 6, h: 1 },
-  ];
-
   return (
     <GridLayout
       className="layout"
-      layout={layout}
       cols={12}
       rowHeight={60}
       width={1200}
-      isResizable={true}
+      isResizable={false}
       isDraggable={true}
+      compactType="vertical"
     >
-      <div key="clock">
+      <div key="clock" data-grid={{ x: 0, y: 0, w: 3, h: 1 }}>
         <Clock />
       </div>
-      <div key="weather">
+      <div key="weather" data-grid={{ x: 3, y: 0, w: 3, h: 2 }}>
         <Weather />
       </div>
-      <div key="news">
+      <div key="news" data-grid={{ x: 0, y: 2, w: 4, h: 2 }}>
         <News />
       </div>
-      <div key="efemerides">
+      <div key="efemerides" data-grid={{ x: 6, y: 0, w: 4, h: 2 }}>
         <Efemerides />
       </div>
-      <div key="feriado">
+      <div key="feriado" data-grid={{ x: 6, y: 2, w: 4, h: 2 }}>
         <NextFeriado />
       </div>
     </GridLayout>
